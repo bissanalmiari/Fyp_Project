e56b2760fc493bb5c5449911ef750b60894fcc99
MasriMM <miramasri112@gmail.com>
2026-04-26 01:01:57 +0300

    added tailwind cdn for student dashboard

--- Unipath/resources/views/student/layout.blade.php
+++ Unipath/resources/views/student/layout.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Unipath - Dashboard</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- link to poppins font -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&family=Rammetto+One&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/ProfileLayout.css') }}">
     @yield('style')
 </head>
